e06191f1c3741ae4f42621511155b3f039b4b9f5
Diogo Evangelista <dhesousa77@gmail.com>
2023-12-29 17:14:33 -0300

    Estrutura visual - Componente: Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logoImg from '../../../public/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <NewTransactionButton>Nova transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
